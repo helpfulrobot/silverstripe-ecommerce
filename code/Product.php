e37a2f6ffaba477b688036b4e1202669eef229c3
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-12-13 08:07:54 +0000

    MINOR: cleaning up some code and adding one method in Order: FixedCountry
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/ssu@1920 950b781c-c544-efab-2f99-90083ee69683

--- code/Product.php
+++ code/Product.php
@@ -98,6 +98,7 @@ class Product extends Page {
 		$fields->replaceField('Root.Content.Main', new HTMLEditorField('Content', _t('Product.DESCRIPTION', 'Product Description'), 3));
 		//NOTE: IMAGE FIELD WAS GIVING ERRORS IN ModelAdmin
 		//$fields->addFieldToTab('Root.Content.Images', new TreeDropdownField('ImageID', _t('Product.IMAGE', 'Product Image'), "Image"));
+		$fields->addFieldToTab('Root.Content.Images', new ImageField('Image', _t('Product.IMAGE', 'Product Image')));
 		$fields->addFieldToTab('Root.Content.Details',new CheckboxField('AllowPurchase', _t('Product.ALLOWPURCHASE', 'Allow product to be purchased'), 1));
 		$fields->addFieldToTab('Root.Content.Details',new CheckboxField('FeaturedProduct', _t('Product.FEATURED', 'Featured Product')));
 		$fields->addFieldToTab('Root.Content.Details',new NumericField('Price', _t('Product.PRICE', 'Price'), '', 12));
