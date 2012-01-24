c66f6a4e4c5a3edde9e2572d9fabc0dd4caded2f
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-01-24 13:39:12 +0000

    ENHANCEMENT: adding a css file for the AccountPage.php
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/ssu@2036 950b781c-c544-efab-2f99-90083ee69683

--- code/AccountPage.php
+++ code/AccountPage.php
@@ -179,6 +179,7 @@ class AccountPage_Controller extends Page_Controller {
 			Security::permissionFailure($this, $messages);
 			return false;
 		}
+		Requirements::themedCSS("AccountPage");
 	}
 
 	/**
