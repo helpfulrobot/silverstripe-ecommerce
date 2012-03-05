0ede34b0427aa02c4d993a3beff249497198f9fd
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-03-05 09:08:11 +0000

    removing a bunch of code that is now part of the OrderFormAddress.js
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/ssu@2171 950b781c-c544-efab-2f99-90083ee69683

--- javascript/EcomOrderForm.js
+++ javascript/EcomOrderForm.js
@@ -11,56 +11,7 @@
 		}
 	);
 	var EcomOrderForm = {
-
-		chars : "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz",
-
-		stringLength : 8,
-
-		passwordFieldInputSelectors: "#PasswordGroup input",
-
-		choosePasswordLinkSelector: ".choosePassword",
-
-		passwordGroupHolderSelector: "#PasswordGroup",
-
-		init: function() {
-			this.passwordInitalisation();
-			this.postalCodeLink();
-		},
-
-		//toggles password selection and enters random password so that users still end up with a password
-		//even if they do not choose one.
-		passwordInitalisation: function() {
-			if(jQuery(EcomOrderForm.passwordFieldInputSelectors).length) {
-				jQuery(EcomOrderForm.choosePasswordLinkSelector).click(
-					function() {
-						jQuery(EcomOrderForm.passwordGroupHolderSelector).toggle();
-						if(jQuery(EcomOrderForm.passwordFieldInputSelectors).is(':visible')) {
-							var newPassword = '';
-						}
-						else{
-							var newPassword = EcomOrderForm.passwordGenerator();
-						}
-						$(EcomOrderForm.passwordFieldInputSelectors).val(newPassword);
-						return false;
-					}
-				);
-				jQuery(EcomOrderForm.choosePasswordLinkSelector).click();
-			}
-		},
-
-		//generates random password
-		passwordGenerator: function() {
-			var randomstring = '';
-			for (var i=0; i < this.stringLength; i++) {
-				var rnum = Math.floor(Math.random() * this.chars.length);
-				randomstring += this.chars.substring(rnum,rnum+1);
-			}
-			return randomstring;
-		},
-
-		postalCodeLink: function() {
-			jQuery(".postalCodeLink").attr("target", "_postalcode");
-		}
+		//TO DO: add T&C checkbox set
 	}
 })(jQuery);
 
