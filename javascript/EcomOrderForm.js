3a2c757db2e67a6ca8e07bbeffcc2ee7af770a1c
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-03-08 19:16:56 +0000

    BUG-FIX: fixing the check of the T and C checkbox
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/ssu@2216 950b781c-c544-efab-2f99-90083ee69683

--- javascript/EcomOrderForm.js
+++ javascript/EcomOrderForm.js
@@ -11,7 +11,31 @@
 		}
 	);
 	var EcomOrderForm = {
-		//TO DO: add T&C checkbox set
+
+		submitButtonSelector: ".Actions input",
+		termsAndConditionsCheckBoxSelector: "#ReadTermsAndConditions input",
+		tAndcMessage: 'You must agree with the terms and conditions to proceed.',
+
+		init: function() {
+			jQuery(EcomOrderForm.submitButtonSelector).click(
+				function(e) {
+					if(!EcomOrderForm.TandCcheck()) {
+						e.preventDefault();
+					}
+				}
+			);
+		},
+
+		TandCcheck: function() {
+			if(jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).length == 1){
+				if(!jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).is(":checked")) {
+					jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).focus();
+					alert(EcomOrderForm.tAndcMessage);
+					return false;
+				}
+			}
+			return true;
+		}
 	}
 })(jQuery);
 
