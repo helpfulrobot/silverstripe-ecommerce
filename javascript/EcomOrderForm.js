e51c2c27e5491377b77d52fc5660ea855ea4dc14
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-03-11 08:13:11 +0000

    ENHANCEMENT: cleaning up JS + ADDING: javascript/EcomSelectOrderAddressField.js
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/ssu@2225 950b781c-c544-efab-2f99-90083ee69683

--- javascript/EcomOrderForm.js
+++ javascript/EcomOrderForm.js
@@ -10,33 +10,33 @@
 			EcomOrderForm.init();
 		}
 	);
-	var EcomOrderForm = {
-
-		submitButtonSelector: ".Actions input",
-		termsAndConditionsCheckBoxSelector: "#ReadTermsAndConditions input",
-		tAndcMessage: 'You must agree with the terms and conditions to proceed.',
-
-		init: function() {
-			jQuery(EcomOrderForm.submitButtonSelector).click(
-				function(e) {
-					if(!EcomOrderForm.TandCcheck()) {
-						e.preventDefault();
-					}
-				}
-			);
-		},
-
-		TandCcheck: function() {
-			if(jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).length == 1){
-				if(!jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).is(":checked")) {
-					jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).focus();
-					alert(EcomOrderForm.tAndcMessage);
-					return false;
-				}
-			}
-			return true;
-		}
-	}
 })(jQuery);
 
 
+var EcomOrderForm = {
+
+	submitButtonSelector: ".Actions input",
+	termsAndConditionsCheckBoxSelector: "#ReadTermsAndConditions input",
+	tAndcMessage: 'You must agree with the terms and conditions to proceed.',
+
+	init: function() {
+		jQuery(EcomOrderForm.submitButtonSelector).click(
+			function(e) {
+				if(!EcomOrderForm.TandCcheck()) {
+					e.preventDefault();
+				}
+			}
+		);
+	},
+
+	TandCcheck: function() {
+		if(jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).length == 1){
+			if(!jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).is(":checked")) {
+				jQuery(EcomOrderForm.termsAndConditionsCheckBoxSelector).focus();
+				alert(EcomOrderForm.tAndcMessage);
+				return false;
+			}
+		}
+		return true;
+	}
+}
--- javascript/EcomOrderFormShipping.js
+++ javascript/EcomOrderFormShipping.js
@@ -10,144 +10,145 @@
 			EcomOrderFormWithShippingAddress.removeEmailFromShippingCityHack();
 		}
 	);
-	var EcomOrderFormWithShippingAddress = {
 
-		firstNameSelector: "#FirstName input",
+})(jQuery);
 
-		shippingFirstNameSelector: "#ShippingFirstName input",
 
-		surnameSelector: "#Surname input",
+var EcomOrderFormWithShippingAddress = {
 
-		shippingSurnameSelector: "#ShippingSurname input",
+	firstNameSelector: "#FirstName input",
 
-		addressSelector: "#Address input",
+	shippingFirstNameSelector: "#ShippingFirstName input",
 
-		extraAddressSelector: "#Address2 input",
+	surnameSelector: "#Surname input",
 
-		shippingAddressSelector: "#ShippingAddress input",
+	shippingSurnameSelector: "#ShippingSurname input",
 
-		shippingExtraAddressSelector: "#ShippingAddress2 input",
+	addressSelector: "#Address input",
 
-		citySelector: "#City input",
+	extraAddressSelector: "#Address2 input",
 
-		shippingCitySelector: "#ShippingCity input",
+	shippingAddressSelector: "#ShippingAddress input",
 
-		postalCodeSelector: "#PostalCode input",
+	shippingExtraAddressSelector: "#ShippingAddress2 input",
 
-		shippingPostalCode: "#ShippingPostalCode input",
+	citySelector: "#City input",
 
-		countrySelector: "#Country select",
+	shippingCitySelector: "#ShippingCity input",
 
-		shippingCountrySelector: "#ShippingCountry select",
+	postalCodeSelector: "#PostalCode input",
 
-		shippingSectionSelector: "#ShippingFields",
+	shippingPostalCode: "#ShippingPostalCode input",
 
-		useShippingDetailsSelector: "input[name='UseShippingAddress']",
+	countrySelector: "#Country select",
 
-		//hides shipping fields
-		//toggle shipping fields when "use separate shipping address" is ticked
-		//update shipping fields, when billing fields are changed.
-		init: function(){
-			//hide shipping fields
-			jQuery(EcomOrderFormWithShippingAddress.shippingSectionSelector).hide();
-			//turn-on shipping details toggle
-			jQuery(EcomOrderFormWithShippingAddress.useShippingDetailsSelector).change(
-				function(){
-					jQuery(EcomOrderFormWithShippingAddress.shippingSectionSelector).slideToggle();
-					jQuery(EcomOrderFormWithShippingAddress.shippingNameSelector).focus();
-					EcomOrderFormWithShippingAddress.updateFields();
-				}
-			);
-			//update on change
-			var originatorFieldSelector =
-					EcomOrderFormWithShippingAddress.firstNameSelector+", "+
-					EcomOrderFormWithShippingAddress.surnameSelector+", "+
-					EcomOrderFormWithShippingAddress.addressSelector+" ,"+
-					EcomOrderFormWithShippingAddress.extraAddressSelector+", "+
-					EcomOrderFormWithShippingAddress.citySelector+", "+
-					EcomOrderFormWithShippingAddress.postalCodeSelector;
-			jQuery(originatorFieldSelector).change(
-				function() {
-					EcomOrderFormWithShippingAddress.updateFields();
-				}
-			).focus(
-				function() {
-					EcomOrderFormWithShippingAddress.updateFields();
-				}
-			);
-			jQuery("input[name='action_processOrder']").click(
-				function() {
-					EcomOrderFormWithShippingAddress.updateFields();
-				}
-			);
-			if(jQuery(EcomOrderFormWithShippingAddress.useShippingDetailsSelector).is(":checked")) {
+	shippingCountrySelector: "#ShippingCountry select",
+
+	shippingSectionSelector: "#ShippingFields, #ShippingFieldsHeader",
+
+	useShippingDetailsSelector: "input[name='UseShippingAddress']",
+
+	//hides shipping fields
+	//toggle shipping fields when "use separate shipping address" is ticked
+	//update shipping fields, when billing fields are changed.
+	init: function(){
+		//hide shipping fields
+		jQuery(EcomOrderFormWithShippingAddress.shippingSectionSelector).hide();
+		//turn-on shipping details toggle
+		jQuery(EcomOrderFormWithShippingAddress.useShippingDetailsSelector).change(
+			function(){
 				jQuery(EcomOrderFormWithShippingAddress.shippingSectionSelector).slideToggle();
 				jQuery(EcomOrderFormWithShippingAddress.shippingNameSelector).focus();
 				EcomOrderFormWithShippingAddress.updateFields();
 			}
-		},
-
-		//copy the billing address details to the shipping address details
-		updateFields: function() {
-			//postal code
-			var PostalCode = jQuery(EcomOrderFormWithShippingAddress.postalCodeSelector).val();
-			var ShippingPostalCode = jQuery(EcomOrderFormWithShippingAddress.shippingPostalCode).val();
-			if(!ShippingPostalCode && PostalCode) {
-				jQuery(EcomOrderFormWithShippingAddress.shippingPostalCode).val(PostalCode);
+		);
+		//update on change
+		var originatorFieldSelector =
+				EcomOrderFormWithShippingAddress.firstNameSelector+", "+
+				EcomOrderFormWithShippingAddress.surnameSelector+", "+
+				EcomOrderFormWithShippingAddress.addressSelector+" ,"+
+				EcomOrderFormWithShippingAddress.extraAddressSelector+", "+
+				EcomOrderFormWithShippingAddress.citySelector+", "+
+				EcomOrderFormWithShippingAddress.postalCodeSelector;
+		jQuery(originatorFieldSelector).change(
+			function() {
+				EcomOrderFormWithShippingAddress.updateFields();
 			}
-
-			//country
-			var Country = jQuery(EcomOrderFormWithShippingAddress.countrySelector).val();
-			var ShippingCountry = jQuery(EcomOrderFormWithShippingAddress.shippingCountrySelector).val();
-			if((!ShippingCountry || ShippingCountry == "AF") && Country) {
-				jQuery(EcomOrderFormWithShippingAddress.shippingCountrySelector).val(Country);
+		).focus(
+			function() {
+				EcomOrderFormWithShippingAddress.updateFields();
 			}
-
-			//city
-			var City = jQuery(EcomOrderFormWithShippingAddress.citySelector).val();
-			var ShippingCity = jQuery(EcomOrderFormWithShippingAddress.shippingCitySelector).val();
-			if(!ShippingCity && City) {
-				jQuery(EcomOrderFormWithShippingAddress.shippingCitySelector).val(City);
+		);
+		jQuery("input[name='action_processOrder']").click(
+			function() {
+				EcomOrderFormWithShippingAddress.updateFields();
 			}
-			//address
-			var Address = jQuery(EcomOrderFormWithShippingAddress.addressSelector).val();
-			var ShippingAddress = jQuery(EcomOrderFormWithShippingAddress.shippingAddressSelector).val();
-			if(!ShippingAddress && Address) {
-				jQuery(EcomOrderFormWithShippingAddress.shippingAddressSelector).val(Address);
-			}
-			//address 2
-			var AddressLine2 = jQuery(EcomOrderFormWithShippingAddress.extraAddressSelector).val();
-			var ShippingAddress2 = jQuery(EcomOrderFormWithShippingAddress.shippingExtraAddressSelector).val();
-			if(!ShippingAddress2 && AddressLine2) {
-				jQuery(EcomOrderFormWithShippingAddress.shippingExtraAddressSelector).val(AddressLine2);
-			}
-			//name
-			var FirstName = jQuery(EcomOrderFormWithShippingAddress.firstNameSelector).val();
-			var ShippingFirstName = jQuery(EcomOrderFormWithShippingAddress.shippingFirstNameSelector).val();
-			if(!ShippingFirstName && FirstName) {
-				jQuery(EcomOrderFormWithShippingAddress.shippingFirstNameSelector).val(FirstName);
-			}
-			var Surname = jQuery(EcomOrderFormWithShippingAddress.SurnameSelector).val();
-			var ShippingSurname = jQuery(EcomOrderFormWithShippingAddress.shippingSurnameSelector).val();
-			if(!ShippingSurname && Surname) {
-				jQuery(EcomOrderFormWithShippingAddress.shippingSurnameSelector).val(Surname);
-			}
-
-		},
-
-		//this function exists, because FF was auto-completing Shipping City as the username part of a password / username combination (password being the next field)
-		removeEmailFromShippingCityHack: function() {
-			var pattern=/^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
-			var shippingCitySelectorValue = jQuery(EcomOrderFormWithShippingAddress.shippingCitySelector).val();
-			if(pattern.test(shippingCitySelectorValue)){
-				jQuery(EcomOrderFormWithShippingAddress.shippingCitySelector).val(jQuery(EcomOrderFormWithShippingAddress.citySelector).val());
-			}
-			else{
-				//do nothing
-			}
-
+		);
+		if(jQuery(EcomOrderFormWithShippingAddress.useShippingDetailsSelector).is(":checked")) {
+			jQuery(EcomOrderFormWithShippingAddress.shippingSectionSelector).slideToggle();
+			jQuery(EcomOrderFormWithShippingAddress.shippingNameSelector).focus();
+			EcomOrderFormWithShippingAddress.updateFields();
 		}
+	},
+
+	//copy the billing address details to the shipping address details
+	updateFields: function() {
+		//postal code
+		var PostalCode = jQuery(EcomOrderFormWithShippingAddress.postalCodeSelector).val();
+		var ShippingPostalCode = jQuery(EcomOrderFormWithShippingAddress.shippingPostalCode).val();
+		if(!ShippingPostalCode && PostalCode) {
+			jQuery(EcomOrderFormWithShippingAddress.shippingPostalCode).val(PostalCode);
+		}
+
+		//country
+		var Country = jQuery(EcomOrderFormWithShippingAddress.countrySelector).val();
+		var ShippingCountry = jQuery(EcomOrderFormWithShippingAddress.shippingCountrySelector).val();
+		if((!ShippingCountry || ShippingCountry == "AF") && Country) {
+			jQuery(EcomOrderFormWithShippingAddress.shippingCountrySelector).val(Country);
+		}
+
+		//city
+		var City = jQuery(EcomOrderFormWithShippingAddress.citySelector).val();
+		var ShippingCity = jQuery(EcomOrderFormWithShippingAddress.shippingCitySelector).val();
+		if(!ShippingCity && City) {
+			jQuery(EcomOrderFormWithShippingAddress.shippingCitySelector).val(City);
+		}
+		//address
+		var Address = jQuery(EcomOrderFormWithShippingAddress.addressSelector).val();
+		var ShippingAddress = jQuery(EcomOrderFormWithShippingAddress.shippingAddressSelector).val();
+		if(!ShippingAddress && Address) {
+			jQuery(EcomOrderFormWithShippingAddress.shippingAddressSelector).val(Address);
+		}
+		//address 2
+		var AddressLine2 = jQuery(EcomOrderFormWithShippingAddress.extraAddressSelector).val();
+		var ShippingAddress2 = jQuery(EcomOrderFormWithShippingAddress.shippingExtraAddressSelector).val();
+		if(!ShippingAddress2 && AddressLine2) {
+			jQuery(EcomOrderFormWithShippingAddress.shippingExtraAddressSelector).val(AddressLine2);
+		}
+		//name
+		var FirstName = jQuery(EcomOrderFormWithShippingAddress.firstNameSelector).val();
+		var ShippingFirstName = jQuery(EcomOrderFormWithShippingAddress.shippingFirstNameSelector).val();
+		if(!ShippingFirstName && FirstName) {
+			jQuery(EcomOrderFormWithShippingAddress.shippingFirstNameSelector).val(FirstName);
+		}
+		var Surname = jQuery(EcomOrderFormWithShippingAddress.SurnameSelector).val();
+		var ShippingSurname = jQuery(EcomOrderFormWithShippingAddress.shippingSurnameSelector).val();
+		if(!ShippingSurname && Surname) {
+			jQuery(EcomOrderFormWithShippingAddress.shippingSurnameSelector).val(Surname);
+		}
+
+	},
+
+	//this function exists, because FF was auto-completing Shipping City as the username part of a password / username combination (password being the next field)
+	removeEmailFromShippingCityHack: function() {
+		var pattern=/^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+		var shippingCitySelectorValue = jQuery(EcomOrderFormWithShippingAddress.shippingCitySelector).val();
+		if(pattern.test(shippingCitySelectorValue)){
+			jQuery(EcomOrderFormWithShippingAddress.shippingCitySelector).val(jQuery(EcomOrderFormWithShippingAddress.citySelector).val());
+		}
+		else{
+			//do nothing
+		}
+
 	}
-})(jQuery);
-
-
+}
